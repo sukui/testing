ea02a66b505b8e91e8808f73789b775688abd5ec
chuxiaofeng <chuxiaofeng@youzan.com>
2017-08-15 15:18:26 +0800

    fix

--- src/UnitTest.php
+++ src/UnitTest.php
@@ -1,34 +1,46 @@
 <?php
 
-namespace ZanPHP\Testing;
-
-class UnitTest extends \PHPUnit_Framework_TestCase
+namespace
 {
-    protected function invoke(&$object, $methodName, array $parameters = [])
-    {
-        $reflection = new \ReflectionClass(get_class($object));
-        $method = $reflection->getMethod($methodName);
-        $method->setAccessible(true);
-
-        return $method->invokeArgs($object, $parameters);
+    // 类别名会触发自动加载
+    // 这里先尝试加载, 没有则新建
+    if (!class_exists("\\PHPUnit_Framework_TestCase", true)) {
+        class PHPUnit_Framework_TestCase {}
     }
+}
 
-    protected function getProperty(&$object, $propertyName)
+
+namespace ZanPHP\Testing
+{
+
+    class UnitTest extends \PHPUnit_Framework_TestCase
     {
-        $reflection = new \ReflectionClass(get_class($object));
-        $property = $reflection->getProperty($propertyName);
-        $property->setAccessible(true);
+        protected function invoke(&$object, $methodName, array $parameters = [])
+        {
+            $reflection = new \ReflectionClass(get_class($object));
+            $method = $reflection->getMethod($methodName);
+            $method->setAccessible(true);
 
-        return $property->getValue($object);
-    }
+            return $method->invokeArgs($object, $parameters);
+        }
 
-    protected function setPropertyValue(&$object, $propertyName, $value)
-    {
-        $reflection = new \ReflectionClass(get_class($object));
-        $property = $reflection->getProperty($propertyName);
-        $property->setAccessible(true);
+        protected function getProperty(&$object, $propertyName)
+        {
+            $reflection = new \ReflectionClass(get_class($object));
+            $property = $reflection->getProperty($propertyName);
+            $property->setAccessible(true);
 
-        return $property->setValue($object, $value);
+            return $property->getValue($object);
+        }
+
+        protected function setPropertyValue(&$object, $propertyName, $value)
+        {
+            $reflection = new \ReflectionClass(get_class($object));
+            $property = $reflection->getProperty($propertyName);
+            $property->setAccessible(true);
+
+            return $property->setValue($object, $value);
+        }
     }
 
 }
